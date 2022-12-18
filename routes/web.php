9fcef861c38b8c282f4e8aa69bc49a4780fabd7f
adiharka <andimahardika73@gmail.com>
2022-12-18 12:22:59 +0700

    Add login page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('client.about');
 })->name('about');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
