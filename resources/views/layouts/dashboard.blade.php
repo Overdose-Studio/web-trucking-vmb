382d3f227a8f3e9cc7fb8136d2b796fc0bd4afae
ubeann <76271600+ubeann@users.noreply.github.com>
2024-06-09 22:53:20 +0700

    feat: create new menu depends on actor

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,16 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>VMB Admin</title>
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css"> -->
-    <!-- Theme style -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+    <!-- Google Font: Source Sans Pro   --> <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome                   --> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Theme style                    --> <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -40,18 +33,14 @@
                         <i class="far fa-user mr-2"></i> {{ Auth::user()->name }}
                     </a>
                     <div class="dropdown-menu dropdown-menu-right">
-                        {{-- <span class="dropdown-item dropdown-header">15 Notifications</span> --}}
-                        {{-- <div class="dropdown-divider"></div> --}}
                         <form action="{{ route('logout') }}" method="post">
                             @csrf
-                            <button type="submit" class="dropdown-item dropdown-footer">Logout</button>
+                            <button type="submit" class="dropdown-item dropdown-footer">
+                                <i class="fas fa-sign-out-alt mr-2"></i>
+                                Logout
+                            </button>
                         </form>
                     </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                        <i class="fas fa-expand-arrows-alt"></i>
-                    </a>
                 </li>
             </ul>
         </nav>
@@ -68,77 +57,122 @@
             <div class="sidebar">
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                        data-accordion="false">
-
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        {{-- Dashboard --}}
                         <li class="nav-item">
                             <a href="{{ route('dashboard') }}"
                                 class="nav-link {{ request()->is('dashboard') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-house"></i>
-                                <p>
-                                    Dashboard
-                                </p>
+                                <p>Dashboard</p>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a href="{{ route('client.index') }}"
-                                class="nav-link {{ request()->is('dashboard/client*') ? 'active' : '' }}">
-                                <i class="nav-icon fas fa-person"></i>
-                                <p>
-                                    Client List
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('truck.index') }}"
-                                class="nav-link {{ request()->is('dashboard/truck*') ? 'active' : '' }}">
-                                <i class="nav-icon fas fa-truck"></i>
-                                <p>
-                                    Trucking
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('dtp.index') }}"
-                                class="nav-link {{ request()->is('dashboard/dtp*') ? 'active' : '' }}">
-                                <i class="nav-icon fas fa-p"></i>
-                                <p>
-                                    DTP
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('dta.index') }}"
-                                class="nav-link {{ request()->is('dashboard/dta*') ? 'active' : '' }}">
-                                <i class="nav-icon fas fa-a"></i>
-                                <p>
-                                    DTA
-                                </p>
-                            </a>
-                        </li>
-                        @if (auth()->user()->role == 'admin' || auth()->user()->role == 'finance')
-                            <li class="nav-item">
-                                <a href="{{ route('bill.index') }}"
-                                    class="nav-link {{ request()->is('dashboard/bill*') ? 'active' : '' }}">
-                                    <i class="nav-icon fas fa-coins"></i>
-                                    <p>
-                                        Finance
-                                    </p>
-                                </a>
-                            </li>
-                        @endif
-                        @if (auth()->user()->role == 'admin')
-                            <li class="nav-item">
-                                <a href="{{ route('user.index') }}"
-                                    class="nav-link {{ request()->is('dashboard/user*') ? 'active' : '' }}">
-                                    <i class="nav-icon fas fa-user"></i>
-                                    <p>
-                                        Account List
-                                    </p>
-                                </a>
-                            </li>
-                        @endif
 
+                        {{-- Admin --}}
+                        <li class="nav-item">
+                            <a href="{{ route('dashboard') }}" class="nav-link">
+                                <i class="nav-icon fas fa-user-shield"></i>
+                                <p>Admin</p>
+                                <i class="right fas fa-angle-left"></i>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{ route('user.index') }}" class="nav-link {{ request()->is('dashboard/user*') ? 'active' : '' }}">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <i class="nav-icon fas fa-edit"></i>
+                                        <p>User Account</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        {{-- Operations --}}
+                        <li class="nav-item">
+                            <a href="{{ route('dashboard') }}" class="nav-link">
+                                <i class="nav-icon fas fa-cogs"></i>
+                                <p>Operation</p>
+                                <i class="right fas fa-angle-left"></i>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{ route('client.index') }}"class="nav-link {{ request()->is('dashboard/client*') ? 'active' : '' }}">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <i class="nav-icon fas fa-person"></i>
+                                        <p>Client</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('dashboard') }}" class="nav-link">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <i class="nav-icon fas fa-plus"></i>
+                                        <p>Shipment</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('dashboard') }}" class="nav-link">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <i class="nav-icon fas fa-check"></i>
+                                        <p>Approval DTA</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        {{-- Trucking --}}
+                        <li class="nav-item">
+                            <a href="{{ route('dashboard') }}" class="nav-link">
+                                <i class="nav-icon fas fa-truck-loading"></i>
+                                <p>Trucking</p>
+                                <i class="right fas fa-angle-left"></i>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{ route('truck.index') }}" class="nav-link {{ request()->is('dashboard/truck*') ? 'active' : '' }}">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <i class="nav-icon fas fa-truck"></i>
+                                        <p>Truck</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('dtp.index') }}" class="nav-link {{ request()->is('dashboard/dtp*') ? 'active' : '' }}">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <i class="nav-icon fas fa-p"></i>
+                                        <p>DTP</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('dta.index') }}" class="nav-link {{ request()->is('dashboard/dta*') ? 'active' : '' }}">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <i class="nav-icon fas fa-a"></i>
+                                        <p>DTA</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        {{-- Finance --}}
+                        <li class="nav-item">
+                            <a href="{{ route('dashboard') }}" class="nav-link">
+                                <i class="nav-icon fas fa-money-check-alt"></i>
+                                <p>Finance</p>
+                                <i class="right fas fa-angle-left"></i>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{ route('dashboard') }}" class="nav-link">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <i class="nav-icon fas fa-check"></i>
+                                        <p>Approval DTP</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('bill.index') }}" class="nav-link {{ request()->is('dashboard/bill*') ? 'active' : '' }}">
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <i class="nav-icon fas fa-coins"></i>
+                                        <p>Bill</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
                     </ul>
                 </nav>
                 <!-- /.sidebar-menu -->
@@ -170,7 +204,6 @@
     <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 
     @yield('modal')
-
 </body>
 
 </html>
