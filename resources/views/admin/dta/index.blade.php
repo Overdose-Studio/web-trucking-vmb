5b1ab36d4850611d9ef9cbd67ad0f25e51b5362f
ubeann <76271600+ubeann@users.noreply.github.com>
2024-06-25 17:39:17 +0700

    feat(dta): improve index page

--- resources/views/admin/dta/index.blade.php
+++ resources/views/admin/dta/index.blade.php
@@ -3,54 +3,101 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-            <h1 class="panel-heading">Daily Trucking Actually List</h1>
+            <div class="d-flex align-items-center">
+                <i class="fas fa-list fa-2x"></i>&nbsp;&nbsp;&nbsp;
+                <h1 class="panel-heading">Daily Trucking Actually List</h1>
+            </div>
         </div>
         <div class="card-body">
             <div class="panel-body">
-                <table class="table table-bordered">
+                <table class="table table-bordered" id="shipment-table">
                     <thead>
                         <tr>
                             <th>Shipment ID</th>
                             <th>Date</th>
                             <th>Order Type</th>
+                            <th>Party</th>
                             <th>Client Name</th>
                             <th>Total Price</th>
+                            <th>Status</th>
                             <th>DTP - DTA</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @forelse ($shipments as $shipment)
+                        @foreach ($shipments as $shipment)
                             <tr>
                                 <td>{{ $shipment->id }}</td>
                                 <td>{{ $shipment->date }}</td>
                                 <td>{{ ucfirst($shipment->order_type) }}</td>
+                                <td>{{ $shipment->party }}</td>
                                 <td>{{ $shipment->client->name }}</td>
                                 <td>Rp {{ number_format($shipment->dailyTruckingActually->sum('price'), 0, ',', '.') }}</td>
                                 <td>Rp {{ number_format($shipment->diff, 0, ',', '.') }}</td>
                                 <td>
+                                    @switch($shipment->status)
+                                        @case("Waiting DTP")
+                                            <span class="badge badge-secondary">
+                                                <i class="fas fa-spinner"></i>&nbsp;
+                                                Waiting DTP
+                                            </span>
+                                            @break
+
+                                        @case("Approving DTP")
+                                            <span class="badge badge-secondary">
+                                                <i class="fas fa-spinner"></i>&nbsp;
+                                                Approving DTP
+                                            </span>
+                                            @break
+
+                                        @case("Waiting DTA")
+                                            <span class="badge badge-success">
+                                                <i class="fas fa-pencil"></i>&nbsp;
+                                                Open
+                                            </span>
+                                            @break
+
+                                        @case("Approving DTA")
+                                            <span class="badge badge-warning">
+                                                <i class="fas fa-spinner"></i>&nbsp;
+                                                Waiting Approval
+                                            </span>
+                                            @break
+
+                                        @default
+                                            <span class="badge badge-danger">
+                                                <i class="fas fa-ban"></i>&nbsp;
+                                                Closed
+                                            </span>
+                                            @break
+                                    @endswitch
+                                </td>
+                                <td>
                                     <a href="{{ route('dta.show', $shipment->id) }}" class="btn btn-primary">Show</a>
-                                    @if (!$shipment->bill_id)
-                                        <a href="{{ route('dta.edit_shipment', $shipment->id) }}"
-                                            class="btn btn-warning">Edit</a>
-                                        <form action="{{ route('dtp.destroy_shipment', $shipment->id) }}" method="POST"
-                                            class="d-inline">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger"
-                                                onclick="return confirm('Are you sure?')">Delete</button>
-                                        </form>
-                                    @endif
                                 </td>
                             </tr>
-                        @empty
-                            <tr>
-                                <td colspan="5" class="text-center">No data available</td>
-                            </tr>
-                        @endforelse
+                        @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
+@endsection
+
+@section('script')
+    <script>
+        $(document).ready(function() {
+            $('#shipment-table').DataTable({
+                responsive: true,
+                autoWidth: false,
+                order: [
+                    [0, 'asc']
+                ],
+                columnDefs: [{
+                    orderable: false,
+                    targets: 8
+                }]
+            });
+        });
+    </script>
 @endsection
