49c47490e6f2691ba96c168d6e753f0c56e64576
ubeann <76271600+ubeann@users.noreply.github.com>
2024-07-05 11:02:08 +0700

    fix(dta): remove `pointer-events` on radio button

--- resources/views/admin/dta/edit.blade.php
+++ resources/views/admin/dta/edit.blade.php
@@ -38,12 +38,12 @@
                             <div class="form-group {{ $errors->has('size') ? 'has-error' : '' }}">
                                 <label>Size</label>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="size" id="size1" value="20" {{ old('size') == 20 || $dta->size == 20 ? 'checked' : '' }} readonly style="pointer-events: none; opacity: 0.5;">
-                                    <label class="form-check-label" for="size1" readonly>20"</label>
+                                    <input class="form-check-input" type="radio" name="size" id="size1" value="20" {{ old('size') == 20 || $dta->size == 20 ? 'checked' : '' }} disabled>
+                                    <label class="form-check-label" for="size1" style="pointer-events: none; opacity: 0.5;">20"</label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="size" id="size2" value="40" {{ old('size') == 40 || $dta->size == 40 ? 'checked' : '' }} readonly style="pointer-events: none;opacity: 0.5;">
-                                    <label class="form-check-label" for="size2" readonly>40"</label>
+                                    <input class="form-check-input" type="radio" name="size" id="size2" value="40" {{ old('size') == 40 || $dta->size == 40 ? 'checked' : '' }} disabled>
+                                    <label class="form-check-label" for="size2" style="pointer-events: none; opacity: 0.5;">40"</label>
                                 </div>
                                 <span class="text-danger">{{ $errors->first('size') }}</span>
                             </div>
