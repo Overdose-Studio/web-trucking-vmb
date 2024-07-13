df2f4fbde1deda9f880b5f902c6b4cfcbf0e1a79
ubeann <76271600+ubeann@users.noreply.github.com>
2024-07-13 20:08:08 +0700

    refactor: update form layout on DTP

--- resources/views/admin/dtp/edit.blade.php
+++ resources/views/admin/dtp/edit.blade.php
@@ -10,8 +10,7 @@
         </div>
         <div class="card-body">
             <div class="panel-body">
-                <form action="{{ route('dtp.update', [$shipment->id, $dtp->id]) }}" method="POST"
-                    enctype="multipart/form-data">
+                <form action="{{ route('dtp.update', [$shipment->id, $dtp->id]) }}" method="POST" enctype="multipart/form-data">
                     {{ csrf_field() }}
                     <div class="form-group {{ $errors->has('driver_name') ? 'has-error' : '' }}">
                         <label for="driver_name">Driver Name</label>
@@ -26,59 +25,7 @@
                         </select>
                         <span class="text-danger">{{ $errors->first('driver_name') }}</span>
                     </div>
-                    <div
-                        class="form-group
-                                {{ $errors->has('destination_1_detail') ? 'has-error' : '' }}">
-                        <label>Destination 1</label>
-                        <input type="text" class="form-control" name="destination_1_detail" placeholder="Destination 1"
-                            value="{{ $dtp->destination_1_id != null ? $dtp->destination1->detail : '' }}">
-                        <span class="text-danger">{{ $errors->first('destination_1_detail') }}</span>
-                    </div>
-                    <div
-                        class="form-group
-                                {{ $errors->has('destination_2_detail') ? 'has-error' : '' }}">
-                        <label>Destination 2</label>
-                        <input type="text" class="form-control" name="destination_2_detail" placeholder="Destination 2"
-                            value="{{ $dtp->destination_2_id != null ? $dtp->destination2->detail : '' }}">
-                        <span class="text-danger">{{ $errors->first('destination_2_detail') }}</span>
-                    </div>
-                    <div
-                        class="form-group
-                                {{ $errors->has('destination_3_detail') ? 'has-error' : '' }}">
-                        <label>Destination 3</label>
-                        <input type="text" class="form-control" name="destination_3_detail" placeholder="Destination 3"
-                            value="{{ $dtp->destination_3_id != null ? $dtp->destination3->detail : '' }}">
-                        <span class="text-danger">{{ $errors->first('destination_3_detail') }}</span>
-                    </div>
-                    <div class="form-group
-                                {{ $errors->has('size') ? 'has-error' : '' }}">
-                        <label>Size</label>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="size" id="size1" value="20"
-                                {{ old('size') == 20 || $dtp->size == 20 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="size1">
-                                20"
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="size" id="size2" value="40"
-                                {{ old('size') == 40 || $dtp->size == 40 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="size2">
-                                40"
-                            </label>
-                        </div>
-                        <span class="text-danger">{{ $errors->first('size') }}</span>
-                    </div>
-                    <div class="form-group
-                                {{ $errors->has('price') ? 'has-error' : '' }}">
-                        <label>Trip Fee</label>
-                        <input type="number" class="form-control" name="price" placeholder="Price"
-                            value="{{ $dtp->price }}">
-                        <span class="text-danger">{{ $errors->first('price') }}</span>
-                    </div>
-                    <div
-                        class="form-group
-                                {{ $errors->has('truck_id') ? 'has-error' : '' }}">
+                    <div class="form-group {{ $errors->has('truck_id') ? 'has-error' : '' }}">
                         <label>Truck</label>
                         <select name="truck_id" class="form-control">
                             <option value="" @if ($dtp->truck_id == null) selected @endif>Vendor Truck</option>
@@ -89,7 +36,42 @@
                         </select>
                         <span class="text-danger">{{ $errors->first('truck_id') }}</span>
                     </div>
-                    <div class="form-group d-flex justify-content-end">
+                    <hr>
+                    <div class="form-group {{ $errors->has('destination_1_detail') ? 'has-error' : '' }}">
+                        <label>Destination 1</label>
+                        <input type="text" class="form-control" name="destination_1_detail" placeholder="Destination 1" value="{{ $dtp->destination_1_id != null ? $dtp->destination1->detail : '' }}">
+                        <span class="text-danger">{{ $errors->first('destination_1_detail') }}</span>
+                    </div>
+                    <div class="form-group {{ $errors->has('destination_2_detail') ? 'has-error' : '' }}">
+                        <label>Destination 2</label>
+                        <input type="text" class="form-control" name="destination_2_detail" placeholder="Destination 2" value="{{ $dtp->destination_2_id != null ? $dtp->destination2->detail : '' }}">
+                        <span class="text-danger">{{ $errors->first('destination_2_detail') }}</span>
+                    </div>
+                    <div class="form-group {{ $errors->has('destination_3_detail') ? 'has-error' : '' }}">
+                        <label>Destination 3</label>
+                        <input type="text" class="form-control" name="destination_3_detail" placeholder="Destination 3" value="{{ $dtp->destination_3_id != null ? $dtp->destination3->detail : '' }}">
+                        <span class="text-danger">{{ $errors->first('destination_3_detail') }}</span>
+                    </div>
+                    <hr>
+                    <div class="form-group {{ $errors->has('size') ? 'has-error' : '' }}">
+                        <label>Size</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="size" id="size1" value="20" {{ old('size') == 20 || $dtp->size == 20 ? 'checked' : '' }}>
+                            <label class="form-check-label" for="size1">20"</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="size" id="size2" value="40" {{ old('size') == 40 || $dtp->size == 40 ? 'checked' : '' }}>
+                            <label class="form-check-label" for="size2">40"</label>
+                        </div>
+                        <span class="text-danger">{{ $errors->first('size') }}</span>
+                    </div>
+                    <div class="form-group {{ $errors->has('price') ? 'has-error' : '' }}">
+                        <label>Trip Fee</label>
+                        <input type="number" class="form-control" name="price" placeholder="Price" value="{{ $dtp->price }}">
+                        <span class="text-danger">{{ $errors->first('price') }}</span>
+                    </div>
+                    <hr>
+                    <div class="form-group d-flex justify-content-end mb-0">
                         <button type="submit" class="btn btn-primary">
                             <i class="fas fa-save"></i>&nbsp;
                             Save Truck
