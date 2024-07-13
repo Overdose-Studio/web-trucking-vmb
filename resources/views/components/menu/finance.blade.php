8347efe8938864f598e4ea668b4807ecdeae0554
Muhammad Rizal <76271600+ubeann@users.noreply.github.com>
2024-07-14 01:59:19 +0700

    feat: adding `logs` menu on other actors (#25)

--- resources/views/components/menu/finance.blade.php
+++ resources/views/components/menu/finance.blade.php
@@ -20,4 +20,12 @@
             <p>Bill</p>
         </a>
     </li>
+
+    {{-- General --}}
+    <li class="nav-item">
+        <a href="{{ route('log.index') }}" class="nav-link {{ request()->is('dashboard/log*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-cogs"></i>
+            <p>Log Shipment</p>
+        </a>
+    </li>
 </ul>
