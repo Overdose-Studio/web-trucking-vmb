46097278640e17f3408a1e1b6a462fc803028a32
ubeann <76271600+ubeann@users.noreply.github.com>
2024-07-14 00:07:35 +0700

    feat: create `logs` table migration

---
+++ database/migrations/2024_07_13_235744_create_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('shipment_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('action');
+            $table->dateTime('date')->useCurrent();
+
+            // Foreign key
+            $table->foreign('shipment_id')->references('id')->on('shipments');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+};
