15ceb3fff5ba5809caa8fe81b0def595e417b0e5
ubeann <76271600+ubeann@users.noreply.github.com>
2024-07-14 01:41:58 +0700

    feat: set carbon locale to `Indonesian` in `AppServiceProvider`

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Code here
     }
 
     /**
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Set carbon locale to Indonesian
+        Carbon::setLocale('id');
     }
 }
