de6571a5b0f2f375537e4e9970998c7cdea1f4fa
ubeann <76271600+ubeann@users.noreply.github.com>
2024-07-14 00:08:05 +0700

    feat: create `logs` enum for type

---
+++ app/Enums/LogType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Enums;
+
+/**
+ * Enum representing types of logs.
+ */
+enum LogType: string
+{
+    /**
+     * Create Shipment log type.
+     *
+     * @var string
+     */
+    case CREATE_SHIPMENT = 'create_shipment';
+
+    /**
+     * Set DTP log type.
+     *
+     * @var string
+     */
+    case SET_DTP = 'set_dtp';
+
+    /**
+     * Approve DTP log type.
+     *
+     * @var string
+     */
+    case APPROVE_DTP = 'approve_dtp';
+
+    /**
+     * Set DTA log type.
+     *
+     * @var string
+     */
+    case SET_DTA = 'set_dta';
+
+    /**
+     * Approve DTA log type.
+     *
+     * @var string
+     */
+    case APPROVE_DTA = 'approve_dta';
+
+    /**
+     * Create Bill log type.
+     *
+     * @var string
+     */
+
+    /**
+     * Unknown log type.
+     *
+     * @var string
+     */
+    case UNKNOWN = 'unknown';
+}
