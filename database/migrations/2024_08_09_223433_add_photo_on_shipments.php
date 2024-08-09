b17cc74e346de207609115545a64c518e3570574
ubeann <76271600+ubeann@users.noreply.github.com>
2024-08-10 01:51:40 +0700

    feat(order): add `photo` attribute on shipment

---
+++ database/migrations/2024_08_09_223433_add_photo_on_shipments.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shipments', function (Blueprint $table) {
+            // Add photo column
+            $table->string('photo')->nullable()->after('party');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shipments', function (Blueprint $table) {
+            // Drop photo column
+            $table->dropColumn('photo');
+        });
+    }
+};
--- resources/views/admin/dtp/create.blade.php
+++ resources/views/admin/dtp/create.blade.php
@@ -1,76 +1,97 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            <div class="d-flex align-items-center">
-                <i class="fas fa-plus fa-2x"></i>&nbsp;&nbsp;&nbsp;
-                <h1 class="panel-heading">Add Truck to DTP</h1>
+    <div class="row">
+        <div class="col-md">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <i class="fas fa-image fa-2x"></i>&nbsp;&nbsp;&nbsp;
+                        <h1 class="panel-heading">Order Reference</h1>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="panel-body">
+                        <a href="{{ asset($shipment->photo) }}" target="_blank">
+                            <img src="{{ asset($shipment->photo) }}" class="img-thumbnail" width="100%">
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="card-body">
-            <div class="panel-body">
-                <form action="{{ route('dtp.store', $shipment->id) }}" method="POST" enctype="multipart/form-data">
-                    {{ csrf_field() }}
-                    {{-- <div class="form-group">
-                        <label for="driver_name">Driver Name</label>
-                        <select name="driver_name" class="form-control">
-                            <option value="" disabled selected>Driver Name</option>
-                            @foreach ($drivers as $driver)
-                                <option value="{{ $driver->name }}" {{ old('driver_name') == $driver->name ? 'selected' : '' }}>
-                                    {{ $driver->name }}
-                                </option>
-                            @endforeach
-                        </select>
-                    </div> --}}
-                    <div class="form-group">
-                        <label for="truck_id">Truck</label>
-                        <select name="truck_id" class="form-control">
-                            <option value="">Vendor Truck</option>
-                            @foreach ($trucks as $truck)
-                                <option value="{{ $truck->id }}" {{ old('truck_id') == $truck->id ? 'selected' : '' }}>
-                                    {{ $truck->license_plate }} | {{ $truck->brand }}
-                                </option>
-                            @endforeach
-                        </select>
+        <div class="col-md">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <i class="fas fa-plus fa-2x"></i>&nbsp;&nbsp;&nbsp;
+                        <h1 class="panel-heading">Add Truck to DTP</h1>
                     </div>
-                    <hr>
-                    <div class="form-group">
-                        <label>Destination 1</label>
-                        <input type="text" class="form-control" name="destination_1_detail" placeholder="Destination 1">
+                </div>
+                <div class="card-body">
+                    <div class="panel-body">
+                        <form action="{{ route('dtp.store', $shipment->id) }}" method="POST" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            {{-- <div class="form-group">
+                                <label for="driver_name">Driver Name</label>
+                                <select name="driver_name" class="form-control">
+                                    <option value="" disabled selected>Driver Name</option>
+                                    @foreach ($drivers as $driver)
+                                        <option value="{{ $driver->name }}" {{ old('driver_name') == $driver->name ? 'selected' : '' }}>
+                                            {{ $driver->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div> --}}
+                            <div class="form-group">
+                                <label for="truck_id">Truck</label>
+                                <select name="truck_id" class="form-control">
+                                    <option value="">Vendor Truck</option>
+                                    @foreach ($trucks as $truck)
+                                        <option value="{{ $truck->id }}" {{ old('truck_id') == $truck->id ? 'selected' : '' }}>
+                                            {{ $truck->license_plate }} | {{ $truck->brand }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <hr>
+                            <div class="form-group">
+                                <label>Destination 1</label>
+                                <input type="text" class="form-control" name="destination_1_detail" placeholder="Destination 1">
+                            </div>
+                            <div class="form-group">
+                                <label>Destination 2</label>
+                                <input type="text" class="form-control" name="destination_2_detail" placeholder="Destination 2">
+                            </div>
+                            <div class="form-group">
+                                <label>Destination 3</label>
+                                <input type="text" class="form-control" name="destination_3_detail" placeholder="Destination 3">
+                            </div>
+                            <hr>
+                            <div class="form-group">
+                                <label for="size">Size</label>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="size" id="size1" value="20" {{ old('size') == 20 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="size1">20"</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="size" id="size2" value="40" {{ old('size') == 40 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="size2">40"</label>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="price">Trip Fee</label>
+                                <input type="number" class="form-control" name="price" placeholder="Price" value="{{ old('price') }}">
+                            </div>
+                            <hr>
+                            <div class="form-group d-flex justify-content-end mb-0">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fas fa-plus"></i>&nbsp;
+                                    Add Truck
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                    <div class="form-group">
-                        <label>Destination 2</label>
-                        <input type="text" class="form-control" name="destination_2_detail" placeholder="Destination 2">
-                    </div>
-                    <div class="form-group">
-                        <label>Destination 3</label>
-                        <input type="text" class="form-control" name="destination_3_detail" placeholder="Destination 3">
-                    </div>
-                    <hr>
-                    <div class="form-group">
-                        <label for="size">Size</label>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="size" id="size1" value="20" {{ old('size') == 20 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="size1">20"</label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="size" id="size2" value="40" {{ old('size') == 40 ? 'checked' : '' }}>
-                            <label class="form-check-label" for="size2">40"</label>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="price">Trip Fee</label>
-                        <input type="number" class="form-control" name="price" placeholder="Price" value="{{ old('price') }}">
-                    </div>
-                    <hr>
-                    <div class="form-group d-flex justify-content-end mb-0">
-                        <button type="submit" class="btn btn-primary">
-                            <i class="fas fa-plus"></i>&nbsp;
-                            Add Truck
-                        </button>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
